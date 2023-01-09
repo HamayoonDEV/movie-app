1e1a079d1236a8eba0f85d380ef439a98d48ad44
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-09 16:26:09 +0500

    added data file to the src folder

---
+++ src/data.js
@@ -0,0 +1,10 @@
+import { getToPathname } from "@remix-run/router";
+
+const data = [
+    {
+        moviename:"pathan",
+        release:2023,
+        ratings:5,
+        imageurl:"https://www.filmibeat.com/img/popcorn/movie_posters/pathaan-20221102160119-19430.jpg"
+    }
+]
